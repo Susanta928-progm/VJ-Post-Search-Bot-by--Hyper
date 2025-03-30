496516802126441eafbb748afa0f704a5eb9b9bd
THE HYPER <susantasarkar1906@gmail.com>
2025-03-30 21:17:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "21134445"))
 API_HASH     = environ.get("API_HASH", "231c18ea7273824491d6bf05425ab74e")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "8194500582:AAENIU65T-Fs6pv4Ez_NqOqAr6PhQIZfY1U")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7783213838:AAEriq09fHAASgN2hdKZ-5s7NifKZvkiL9w")
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://bornhyper1Bornhyper5911@cluster0.gmvpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002421090499"))
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002166830074"))
 ADMIN        = int(environ.get("ADMIN", "7763229951"))
-CHANNEL      = environ.get("CHANNEL", "@ANIMECHILLAX")
+CHANNEL      = environ.get("CHANNEL", "@ACX_NETWORK")
